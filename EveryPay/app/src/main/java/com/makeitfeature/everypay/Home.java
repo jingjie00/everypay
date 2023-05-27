651d353140b0e33d8052fd29f1609703e7e46462
Weave-Trix <malaysia.airlines.journal@gmail.com>
2023-05-27 23:31:37 +0800

    ADDED: Action Bar

--- EveryPay/app/src/main/java/com/makeitfeature/everypay/Home.java
+++ EveryPay/app/src/main/java/com/makeitfeature/everypay/Home.java
@@ -1,13 +1,19 @@
 package com.makeitfeature.everypay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.makeitfeature.everypay.ui.settings.PreferenceActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -17,13 +23,32 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_home_sidebar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                // redirect to settings page
+                Intent intent = new Intent(this, PreferenceActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
