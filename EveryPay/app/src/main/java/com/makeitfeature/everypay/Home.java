9492390fbfb023b1e36dcc85a20a62099840d1f3
Weave-Trix <malaysia.airlines.journal@gmail.com>
2023-05-27 19:52:33 +0800

    REFACTOR: rename res/layout

--- EveryPay/app/src/main/java/com/makeitfeature/everypay/Home.java
+++ EveryPay/app/src/main/java/com/makeitfeature/everypay/Home.java
@@ -3,15 +3,11 @@ package com.makeitfeature.everypay;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
 
@@ -20,7 +16,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.main_home_sidebar);
     }
 
     @Override
